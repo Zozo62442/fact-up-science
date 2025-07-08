ceef5a5a7d8bfc66560731c0797e81ab9528061b
Zozo62442 <zoe.rotterr@gmail.com>
2025-07-08 15:24:41 +0200

    create Procfile

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-x$xt+%yaov&uxy@ra7sj)*)d-q24rg!55-e)4-$gi=!l+1&7=&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["herokuapp.com", "127.0.0.1",]
 
 
 # Application definition
