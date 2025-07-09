97d08f9ce9f1f21a7efbbdba4d98f0b91149a5ec
Zozo62442 <zoe.rotterr@gmail.com>
2025-07-09 10:42:54 +0200

    update procfile

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ["herokuapp.com", "127.0.0.1", "fact-up-science-blog-da75ad178a21.herokuapp.com"]
+ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
