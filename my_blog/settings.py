1a56ae8455951537c0aa483fd0b205da64d5af5e
Zozo62442 <zoe.rotterr@gmail.com>
2025-07-09 10:34:52 +0200

    Update settings and requirements files

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x$xt+%yaov&uxy@ra7sj)*)d-q24rg!55-e)4-$gi=!l+1&7=&"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ["herokuapp.com", "127.0.0.1",]
+ALLOWED_HOSTS = ["herokuapp.com", "127.0.0.1", "fact-up-science-blog-da75ad178a21.herokuapp.com"]
 
 
 # Application definition
@@ -77,11 +81,15 @@ WSGI_APPLICATION = "my_blog.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
