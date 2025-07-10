fc247cc2cde4be5252138f408d6a43fc0042466f
Zozo62442 <zoe.rotterr@gmail.com>
2025-07-10 15:53:51 +0200

    add base template and homepage

--- fact_up_blog/views.py
+++ fact_up_blog/views.py
@@ -14,4 +14,5 @@ def index(request):
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = 'blog/post_list.html'
+    template_name = "blog/index.html"
+    paginate_by = 6
